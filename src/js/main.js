ee45352a62a0a63172d9a4019f13e97ecaeab086
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-01-02 19:45:08 +0100

    initial commit, starting project

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+	window.addEventListener('load', (e) => {
+		const preload = document.querySelector('.preload');
+
+		preload.classList.add('preload-finished');
+	});
+});
