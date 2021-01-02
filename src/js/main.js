bcc49e54a8157c50404c8aa6f0d4d8214631833c
Francuski Miroslav <francuski.miroslav@gmail.com>
2021-01-02 23:52:44 +0100

    draw fillstyle needs fix for rgb values

--- src/js/main.js
+++ src/js/main.js
@@ -16,21 +16,54 @@ document.addEventListener('DOMContentLoaded', function () {
 		canvas.height = 540;
 
 		ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+		const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		let particlesArray = [];
 		const numberOfParticles = 5000;
+
+		let mappedImage = [];
+		for (let y = 0; y < canvas.height; y++) {
+			let row = [];
+			for (let x = 0; x < canvas.width; x++) {
+				const red = pixels.data[y * 4 * pixels.width + x * 4];
+				const green = pixels.data[y * 4 * pixels.width + (x * 4 + 1)];
+				const blue = pixels.data[y * 4 * pixels.width + (x * 4 + 2)];
+				const brightness = calculateRealtiveBrightness(red, green, blue);
+				const cell = [(cellBrightness = brightness)];
+
+				row.push(cell);
+			}
+
+			mappedImage.push(row);
+		}
+
+		function calculateRealtiveBrightness(red, green, blue) {
+			return (
+				Math.sqrt(
+					red * red * 0.299 + green * green * 0.587 + blue * blue * 0.114
+				) / 100
+			);
+		}
 
 		class Particle {
 			constructor() {
 				this.x = Math.random() * canvas.width;
 				this.y = 0;
 				this.speed = 0;
-				this.velocity = Math.random() * 3.5;
+				this.velocity = Math.random() * 0.5;
 				this.size = Math.random() * 1.5 + 1;
+				this.position1 = Math.floor(this.y);
+				this.position2 = Math.floor(this.x);
 			}
 
 			update() {
-				this.y += this.velocity;
+				this.position1 = Math.floor(this.y);
+				this.position2 = Math.floor(this.x);
+				this.speed = mappedImage[this.position1][this.position2][0];
+				let movement = 2.5 - this.speed + this.velocity;
+
+				this.y += movement;
 
 				if (this.y >= canvas.height) {
 					this.y = 0;
@@ -40,7 +73,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 			draw() {
 				ctx.beginPath();
-				ctx.fillStyle = 'white';
+
+				ctx.fillStyle = `rgb(200, 150, 100)`;
+
 				ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
 				ctx.fill();
 			}
@@ -56,10 +91,13 @@ document.addEventListener('DOMContentLoaded', function () {
 
 		function animate() {
 			ctx.globalAlpha = 0.05;
-			ctx.fillStyle = 'rgb(0,0,0)';
+			ctx.fillStyle = 'rgb(50, 50, 50)';
 			ctx.fillRect(0, 0, canvas.width, canvas.height);
+			ctx.drawImage(myImage, 0, 0, canvas.width, canvas.height);
+			ctx.globalAlpha = 0.2;
 			particlesArray.forEach((particle) => {
 				particle.update();
+				ctx.globalAlpha = particle.speed * 0.5;
 				particle.draw();
 			});
 			requestAnimationFrame(animate);
